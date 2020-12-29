f98d5252b31010354ad34fa41a0108a14e9aa7f8
bicod <50411342+bicod@users.noreply.github.com>
2020-12-29 16:32:59 +0530

    Update DCP_12.java

--- DCP_12.java
+++ DCP_12.java
@@ -2,17 +2,11 @@ package com.company;
 import org.jetbrains.annotations.Contract;
 
 class DCP_12 {
-    static int countWays(int n, int m)
+    static int countWays(int n)
     {
-        int res[] = new int[n];
-        res[0] = 1;
-        res[1] = 1;
-        for (int i = 2; i < n; i++) {
-            res[i] = 0;
-            for (int j = 1; j <= m && j <= i; j++)
-                res[i] += res[i - j];
-        }
-        return res[n - 1];
+        if(n<=1)
+            return n;
+        return countWays(n-1)+countWays(n-2);
     }
 }
 public class Main{
@@ -20,9 +14,9 @@ public class Main{
 
 
         // Driver method
-        int s = 4, m = 2;
+        int s = 4;
         System.out.println("Number of ways = "
-                + DCP_12.countWays(s+1, m));
+                + DCP_12.countWays(s+1));
 
     }
 }
